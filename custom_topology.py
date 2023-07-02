761d2d8298abf1d6544d5f4f730d44c0650ff070
Rusheel Raj Panakadan <30828807+Rusheelraj@users.noreply.github.com>
2023-07-02 16:36:38 -0400

    Add files via upload

---
+++ custom_topology.py
@@ -0,0 +1,42 @@
+from mininet.net import Mininet
+from mininet.node import Controller
+from mininet.cli import CLI
+from mininet.log import setLogLevel, info
+from mininet.node import Controller, RemoteController  # add RemoteController here
+
+
+def customNet():
+    "Create a custom network with 3 hosts and a switch"
+
+    net = Mininet(controller=Controller)
+
+    info('*** Adding controller\n')
+#    net.addController('c0')
+    net.addController('c0', controller=RemoteController, ip="127.0.0.1", port=6633)
+
+    info('*** Adding hosts\n')
+    h1 = net.addHost('h1', ip='10.0.0.1')
+    h2 = net.addHost('h2', ip='10.0.0.2')
+    h3 = net.addHost('h3', ip='10.0.0.3')  # this is your new host
+
+    info('*** Adding switch\n')
+    s1 = net.addSwitch('s1')
+
+    info('*** Creating links\n')
+    net.addLink(h1, s1)
+    net.addLink(h2, s1)
+    net.addLink(h3, s1)  # link the new host to the switch
+
+    info('*** Starting network\n')
+    net.start()
+
+    info('*** Running CLI\n')
+    CLI(net)
+
+    info('*** Stopping network')
+    net.stop()
+
+
+if __name__ == '__main__':
+    setLogLevel('info')
+    customNet()
